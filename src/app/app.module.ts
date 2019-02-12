20ac5b84cfab3ddd9d78ca4f46db9b0106f662ea
ChanoHL <shanohl.sist@gmail.com>
2019-02-12 15:08:30 -0500

    routing app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+// import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { ProgressComponent } from './pages/progress/progress.component';
+// import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { APP_ROUTER } from './app.routes';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register/register.component';
+
+//modulo;
+import { PagesModule } from './pages/pages.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    // DashboardComponent,
+    // ProgressComponent,
+    // Graficas1Component,
+    // HeaderComponent,
+    // SidebarComponent,
+    // BreadcrumbsComponent,
+    //PagesComponent,
+    RegisterComponent    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    APP_ROUTER,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
